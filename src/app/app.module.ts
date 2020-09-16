8ae124daefe18ab9081524aea40d99f517af01f1
kirandivate <divate26@gmail.com>
2020-09-16 18:52:40 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -35,6 +35,7 @@ import { Methods } from './_Model/methods';
     Login,
     Methods
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
